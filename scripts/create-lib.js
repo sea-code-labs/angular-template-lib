44be96cefa3ca172c522e342d495efcc6f1a82a8
Kacper Lechicki <klechicki.code@gmail.com>
2024-09-01 09:28:46 +0200

    SC-21 refactor: cleanup pipeline 1.0.0

--- scripts/create-lib.js
+++ scripts/create-lib.js
@@ -4,46 +4,40 @@ const { execSync } = require('child_process');
 const fs = require('fs');
 // Import the path module for handling file paths
 const path = require('path');
-// Retrieve the application name from the command-line arguments
+// Retrieve the library name from the command-line arguments
 const [, , name] = process.argv;
 
-// Check if the application name was provided
+// Check if the library name was provided
 if (!name) {
-  // Display an error message if no application name is provided
-  console.error('Please provide the name of the application.');
+  // Display an error message if no library name is provided
+  console.error('Please provide the name of the library.');
   // Exit the process with an error code 1
   process.exit(1);
 }
 
 try {
-  // Execute the command to generate a new Angular application with specified options
-  execSync(`ng generate application ${name} --project-root=projects/apps/${name} --style=scss`, { stdio: 'inherit' });
-
+  // Execute the command to generate a new Angular library with specified options
+  execSync(`ng generate library ${name} --project-root=projects/libs/${name}`, { stdio: 'inherit' });
   // Create the path to the angular.json file
   const angularJsonPath = path.join(process.cwd(), 'angular.json');
   // Read and parse the content of angular.json
   const angularJson = JSON.parse(fs.readFileSync(angularJsonPath, 'utf-8'));
 
-  // Check if the project exists in angular.json
+  // Check if the library exists in angular.json
   if (angularJson.projects && angularJson.projects[name]) {
     // Add the path to the karma configuration file
     angularJson.projects[name].architect.test.options.karmaConfig = 'karma.conf.js';
-
-    // Add the prefix to the project's configuration
-    angularJson.projects[name].prefix = 'sc';
-
     // Write the updated content back to angular.json
     fs.writeFileSync(angularJsonPath, JSON.stringify(angularJson, null, 2));
-
-    // Display a message indicating the karma configuration and prefix were successfully added
-    console.log(`Added karmaConfig and prefix to ${name} in angular.json`);
+    // Display a message indicating the karma configuration was successfully added
+    console.log(`Added karmaConfig to ${name} in angular.json`);
   } else {
-    // Display an error message if the application was not found in angular.json
-    console.error(`Application ${name} not found in angular.json`);
+    // Display an error message if the library was not found in angular.json
+    console.error(`Library ${name} not found in angular.json`);
   }
 } catch (error) {
   // Handle errors that occur during the process
-  console.error('Error generating application or updating angular.json:', error.message);
+  console.error('Error generating library or updating angular.json:', error.message);
   // Exit the process with an error code 1
   process.exit(1);
 }
