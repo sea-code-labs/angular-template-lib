db1aaecbcf19cf0337874fac47b1d9fcc8631f2d
Kacper Lechicki <klechicki.code@gmail.com>
2024-09-01 12:01:06 +0200

    SC-21 refactor: cleanup pipeline 1.0.8

--- scripts/delete-merged-branches.sh
+++ scripts/delete-merged-branches.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-git fetch --all
-
-echo "Listing all merged feature branches..."
+git fetch --all --prune
+git branch -a
+git checkout develop
+git branch -r --merged origin/develop
 
 # Find merged feature branches and delete them
 merged_branches=$(git branch -r --merged origin/develop | grep 'origin/feature-' | sed 's/origin\///')
