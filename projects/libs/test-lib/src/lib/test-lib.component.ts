156d862da4682842f29aeb1fd0df97ba7e4e566f
Kacper Lechicki <klechicki.code@gmail.com>
2024-09-02 19:03:26 +0200

    SC-23 feat: pipeline mirror 1.0.0

--- projects/libs/test-lib/src/lib/test-lib.component.ts
+++ projects/libs/test-lib/src/lib/test-lib.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sc-test-lib',
-  template: '<p>test-lib works!</p>',
   standalone: true,
+  imports: [],
+  template: ' <p>test-lib works!</p> ',
+  styles: '',
 })
 export class TestLibComponent {}
